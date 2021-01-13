0d2a7b7fd2e77e1292092b561a8689208805e16a
Alejandro-Ochando <alejandro.ochando@live.com>
2021-01-13 19:31:52 +0100

    Añadiendo el Header  y preparando el Form

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      
+      <div className="container mt-5">
+        <div className="row">
+          <Form />
+        </div>
+      </div>
+
+    </Fragment>
+  );
+}
+
+export default App;
