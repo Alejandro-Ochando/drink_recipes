4903fb8e3cc85cb0db4128a439446c7caaf55e8f
Alejandro-Ochando <alejandro.ochando@live.com>
2021-01-15 18:07:33 +0100

    Importano el componente Modal

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import axios from 'axios';
 
+
 export const ModalContext = createContext();
 
 const ModalProvider = (props) => {
